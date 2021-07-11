6a17e56f3be160fbd1f885a79b2e7b9619bcc666
el3031 <67562724+el3031@users.noreply.github.com>
2021-07-11 18:10:55 -0400

    got  the audio rings to play on click, without using UI

--- Assets/Scripts/RingOrientation.cs
+++ Assets/Scripts/RingOrientation.cs
@@ -5,15 +5,21 @@ using UnityEngine;
 public class RingOrientation : MonoBehaviour
 {
     // Start is called before the first frame update
+    private AudioSource audioSource;
     void Start()
     {
-        
+        audioSource = GetComponentInChildren<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetMouseButtonDown(0))
+        {
+            audioSource.Play();
+        }
+        
         Quaternion cameraRotation = Camera.main.transform.rotation;
-        transform.rotation = Quaternion.Slerp(transform.rotation, cameraRotation, Time.deltaTime);
+        transform.rotation = Quaternion.Slerp(transform.rotation, cameraRotation, 0.125f);
     }
 }
