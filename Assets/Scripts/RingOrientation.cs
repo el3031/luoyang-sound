f9d59b7eb7c9ded63bff999a32f2ab22d67323c6
el3031 <67562724+el3031@users.noreply.github.com>
2021-07-10 22:55:01 -0400

    scrippt for ring rotation

---
+++ Assets/Scripts/RingOrientation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RingOrientation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Quaternion cameraRotation = Camera.main.transform.rotation;
+        transform.rotation = Quaternion.Slerp(transform.rotation, cameraRotation, Time.deltaTime);
+    }
+}
