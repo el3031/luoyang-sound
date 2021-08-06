75791862a5e214032042c47698f239afb74dd5e2
el3031 <67562724+el3031@users.noreply.github.com>
2021-08-06 11:24:19 -0700

    got the button UI and background to work

---
+++ Assets/Scripts/BackgroundScale.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class BackgroundScale : MonoBehaviour {
+    
+    private Image img;
+    private float sHeight;
+    private float sWidth;
+
+    void Start()
+    {
+        img = GetComponent<Image>();
+        sHeight = img.sprite.bounds.size.y;
+        sWidth = img.sprite.bounds.size.x;
+    }
+    void Update()
+    {
+        float width = ScreenSize.GetScreenToWorldWidth;
+        float height = ScreenSize.GetScreenToWorldHeight;
+        float hRatio = height / sHeight;
+        float wRatio = width / sWidth;
+        transform.localScale =  Vector3.one * ((wRatio > hRatio) ? width : height);
+    }
+}
